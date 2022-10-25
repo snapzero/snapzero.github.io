d80d54b2caed111ffaad5015b852ba22d3a714b2
Brandon Blair <lbrandon.blair@gmail.com>
2022-10-25 14:49:06 -0400

    a few temp fixes

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ $(document).on('click', '.menu-icon', function(){
 
 /*--------- Add header background on scroll ----------*/
 $(function() {
-  var pagePos = function() {
-                  var pageY = $(window).scrollTop();
+  const pagePos = function() {
+                  const pageY = $(window).scrollTop();
                   pageY > 1 ? $(".menu").addClass("menu-bg") : $(".menu").removeClass("menu-bg");
                   return pageY;
                 };
@@ -24,7 +24,7 @@ $(function() {
 
 /*--------- Scroll to hash ----------*/
 $(document).on('click touchend', 'a[href^="#"]', function(e) {
-  var link = $(this).attr('href');
+  const link = $(this).attr('href');
   if($('.menu').hasClass('menu-open')) {
     $('.menu-icon').removeClass('is-active');
     $('.menu').toggleClass('menu-open');
@@ -36,21 +36,21 @@ $(document).on('click touchend', 'a[href^="#"]', function(e) {
 });
 
 /*--------- Active Link Scroll ----------*/
-var lastId,
-    topMenu = $(".menu-list"),
-    topMenuHeight = topMenu.outerHeight()+15,
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-      var item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
+let lastId
+let topMenu = $(".menu-list")
+let topMenuHeight = topMenu.outerHeight()
+// All list items
+let menuItems = topMenu.find("a")
+// Anchors corresponding to menu items
+const scrollItems = menuItems.map(function() {
+  let item = $($(this).attr("href"));
+  if (item.length) { return item; }
+});
 
 // Bind click handler to menu items for fancy scroll animation
-menuItems.on('click touchend', function(){
-  var href = $(this).attr("href"),
-  offsetTop = href === "#" ? 0 : $(href).offset().top;/*--- Uncomment for fancy scrolling //-topMenuHeight+1; ---*/
+menuItems.on('click touchend', function() {
+  const href = $(this).attr("href");
+  const offsetTop = href === "#" ? 0 : $(href).offset().top;/*--- Uncomment for fancy scrolling //-topMenuHeight+1; ---*/
   if ( !isMobileWidth() ) {
     $('html, body').stop().animate({
         scrollTop: offsetTop
@@ -60,24 +60,25 @@ menuItems.on('click touchend', function(){
 });
 
 // Bind to scroll
-$(window).scroll(function(){
- // Get container scroll position
- var fromTop = $(this).scrollTop()+topMenuHeight;
+$(window).on('resize scroll', function() {
 
- // Get id of current scroll item
- var cur = scrollItems.map(function(){
-   if ($(this).offset().top < fromTop)
-     return this;
- });
- // Get the id of the current element
- cur = cur[cur.length-1];
- var id = cur && cur.length ? cur[0].id : "";
+  // Get id of current scroll item
+  let cur = scrollItems.map(function(item) {
+    const viewportTop = $(window).scrollTop() + topMenuHeight;
+    if ($(this).offset().top < viewportTop) return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length - 1];
+  const id = cur && cur.length ? cur[0].id : "";
 
- if (lastId !== id) {
-     lastId = id;
-     // Set/remove active class
-     menuItems
-       .parent().removeClass("menu-selected")
-       .end().filter("[href=#"+id+"]").parent().addClass("menu-selected");
- }
+  if (lastId !== id) {
+    lastId = id;
+    // Set/remove active class
+    menuItems
+      .parent().removeClass("menu-selected")
+      .end()
+      .filter(`[href='#${id}']`)
+      .parent()
+      .addClass("menu-selected");
+  }
 });
